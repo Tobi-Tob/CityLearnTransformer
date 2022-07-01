6469aa0259aeb130d746945c72238151f8b2a47d
Dipam Chakraborty <dipamc77@gmail.com>
2022-07-01 16:26:00 +0530

    added local evaluation

---
+++ data/README.md
@@ -0,0 +1,5 @@
+# Environment Schema
+
+Citylearn settings are controlled by schema files, such as `citylearn_challenge_2022_phase_1/schema.json`.
+
+Feel free to modify the schema for your local evalution as you see fit, the evaluator will **not** use any schema you add here.
---
+++ local_evaluation.py
@@ -0,0 +1,87 @@
+import numpy as np
+import time
+
+from agents.orderenforcingwrapper import OrderEnforcingAgent
+from citylearn.citylearn import CityLearnEnv
+
+class Constants:
+    episodes = 3
+    schema_path = './data/citylearn_challenge_2022_phase_1/schema.json'
+
+def action_space_to_dict(aspace):
+    """ Only for box space """
+    return { "high": aspace.high,
+             "low": aspace.low,
+             "shape": aspace.shape,
+             "dtype": str(aspace.dtype)
+    }
+
+def env_reset(env):
+    observations = env.reset()
+    action_space = env.action_space
+    action_space_dicts = [action_space_to_dict(asp) for asp in action_space]
+    obs_dict = {"action_space": action_space_dicts,
+                "observation": observations }
+    return obs_dict
+
+def evaluate():
+    print("Starting local evaluation")
+    
+    env = CityLearnEnv(schema=Constants.schema_path)
+    agent = OrderEnforcingAgent()
+
+    obs_dict = env_reset(env)
+
+    agent_time_elapsed = 0
+
+    step_start = time.perf_counter()
+    actions = agent.register_reset(obs_dict)
+    agent_time_elapsed += time.perf_counter()- step_start
+
+    episodes_completed = 0
+    num_steps = 0
+    interrupted = False
+    episode_metrics = []
+    try:
+        while True:
+            observations, _, done, _ = env.step(actions)
+            if done:
+                episodes_completed += 1
+                metrics_t = env.evaluate()
+                metrics = {"price_cost": metrics_t[0], "emmision_cost": metrics_t[1]}
+                if np.any(np.isnan(metrics_t)):
+                    raise ValueError("Episode metrics are nan, please contant organizers")
+                episode_metrics.append(metrics)
+                print(f"Episode complete: {episodes_completed} | Latest episode metrics: {metrics}", )
+
+                obs_dict = env_reset(env)
+
+                step_start = time.perf_counter()
+                actions = agent.register_reset(obs_dict)
+                agent_time_elapsed += time.perf_counter()- step_start
+            else:
+                step_start = time.perf_counter()
+                actions = agent.compute_action(observations)
+                agent_time_elapsed += time.perf_counter()- step_start
+            
+            num_steps += 1
+            if num_steps % 1000 == 0:
+                print(f"Num Steps: {num_steps}, Num episodes: {episodes_completed}")
+
+            if episodes_completed >= Constants.episodes:
+                break
+    except KeyboardInterrupt:
+        print("========================= Stopping Evaluation =========================")
+        interrupted = True
+    
+    if not interrupted:
+        print("=========================Completed=========================")
+
+    if len(episode_metrics) > 0:
+        print("Average Price Cost:", np.mean([e['price_cost'] for e in episode_metrics]))
+        print("Average Emmision Cost:", np.mean([e['emmision_cost'] for e in episode_metrics]))
+    print(f"Total time taken by agent: {agent_time_elapsed}s")
+    
+
+if __name__ == '__main__':
+    evaluate()
