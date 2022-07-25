f7218337cbd0e0c92cd56837b4f2067cf1b42853
Kingsley Nweye <etonwana@yahoo.com>
2022-07-25 15:18:26 -0500

    fixed type bug

--- local_evaluation.py
+++ local_evaluation.py
@@ -28,9 +28,11 @@ def env_reset(env):
     action_space = env.action_space
     observation_space = env.observation_space
     building_info = env.get_building_information()
+    building_info = list(building_info.values())
     action_space_dicts = [action_space_to_dict(asp) for asp in action_space]
+    observation_space_dicts = [action_space_to_dict(osp) for osp in observation_space]
     obs_dict = {"action_space": action_space_dicts,
-                "observation_space": observation_space,
+                "observation_space": observation_space_dicts,
                 "building_info": building_info,
                 "observation": observations }
     return obs_dict
