ff9a1ea1b32310b9e0ef18b1f3d4f67598583a97
Adrien B <forbu14@gmail.com>
2022-10-01 17:50:52 +0200

    final correction

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -9,6 +9,39 @@ import sys
 
 import numpy as np
 
+str_to_int_mapping = {
+    "month"  :   0,
+"day_type"  :    1,
+"hour"  :    2,
+"outdoor_dry_bulb_temperature"  :  3,
+"outdoor_dry_bulb_temperature_predicted_6h"  :    4,
+"outdoor_dry_bulb_temperature_predicted_12h"  :    5,
+"outdoor_dry_bulb_temperature_predicted_24h"  :    6,
+"outdoor_relative_humidity"  :   7,
+"outdoor_relative_humidity_predicted_6h"  :   8,
+"outdoor_relative_humidity_predicted_12h"  :    9,
+"outdoor_relative_humidity_predicted_24h"  :  10,
+"diffuse_solar_irradiance"  :    11,
+"diffuse_solar_irradiance_predicted_6h"  :   12,
+"diffuse_solar_irradiance_predicted_12h"  :   13,
+"diffuse_solar_irradiance_predicted_24h"  :   14,
+"direct_solar_irradiance"  : 15,
+"direct_solar_irradiance_predicted_6h"  :   16,
+"direct_solar_irradiance_predicted_12h"  :  17,
+"direct_solar_irradiance_predicted_24h"  :  18,
+"carbon_intensity"  :  19,
+"non_shiftable_load"  :  20,
+"solar_generation"  :  21,
+"electrical_storage_soc"  : 22,
+"net_electricity_consumption"  :  23,
+"electricity_pricing"  :   24,
+"electricity_pricing_predicted_6h"  : 25,
+"electricity_pricing_predicted_12h"  :  26,
+"electricity_pricing_predicted_24h"  : 27,
+}
+
+int_to_str_mapping = {v: k for k, v in str_to_int_mapping.items()}
+
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
@@ -72,7 +105,7 @@ class UserAgent:
         """Get observation return action"""
 
         # add observation to the history
-        for var in range(len(self.variables_building)):
+        for var in self.variables_building:
             self.history[agent_id][var].append(observation[var])
     
     def get_model_prediction(self, agent_id):
@@ -95,13 +128,29 @@ class UserAgent:
                 input_data = np.zeros((self.lookback_info, ))*np.nan
                 input_data[-data[var].shape[1]:] = data[var]
 
-            prediction_t[var] = self.models[var].predict(data[var])[0]
+            prediction_t[var] = self.models[var].predict(input_data.reshape((1, -1)))[0]
 
         # we get the model prediction
         return prediction_t
+
+    def augment_observations(self, observation, agent_id):
+
+        observation_dict = {}
+
+        for idx, obs in enumerate(observation):
+            observation_dict[int_to_str_mapping[idx]] = obs
+
+        # adding crude electricity consumption and index pricing in observation
+        observation_dict['electricity_consumption_crude'] = observation_dict['non_shiftable_load'] - observation_dict['solar_generation']
+        observation_dict['index_pricing'] = observation_dict['electricity_pricing'] + observation_dict['carbon_intensity']
+
+        return observation_dict
     
     def compute_action(self, observation, agent_id):
         """Get observation return action"""
+
+        # we augment the observation with the crude electricity consumption and the index pricing
+        observation = self.augment_observations(observation, agent_id)
 
         # add observation to the history
         self.adding_observation(observation, agent_id)
@@ -112,4 +161,4 @@ class UserAgent:
         # now we can compute the action with rule based behaviour
         action = simple_rule_optim(prediction_t)
 
-        return action
+        return [action]
