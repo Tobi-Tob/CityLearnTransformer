cabfa598138354f9580c351a54af70f6fb67a291
Tobi <tobi.lingenberg@t-online.de>
2023-06-29 13:44:44 +0200

    minor changes

--- run_experiments.py
+++ run_experiments.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
     from evaluate_DT import evaluate
 
-    evaluate(DT_model="TobiTob/decision_transformer_fr_24", buildings_to_use="validation",
-             TR=-9000, evaluation_interval=24, simulation_start_end=[0, 8759], file_to_save='_results1.txt')
+    evaluate(DT_model="TobiTob/decision_transformer_merged1", buildings_to_use="validation",
+             TR=-9000, evaluation_interval=168, simulation_start_end=[0, 8759], file_to_save='_results100_9000.txt')
+    evaluate(DT_model="TobiTob/decision_transformer_merged1", buildings_to_use="validation",
+             TR=-6000, evaluation_interval=168, simulation_start_end=[0, 8759], file_to_save='_results100_6000.txt')
+    evaluate(DT_model="TobiTob/decision_transformer_merged1", buildings_to_use="validation",
+             TR=-12000, evaluation_interval=168, simulation_start_end=[0, 8759], file_to_save='_results100_12000.txt')
 
-    evaluate(DT_model="TobiTob/decision_transformer_fr_24", buildings_to_use="test",
-             TR=-9000, evaluation_interval=24, simulation_start_end=[0, 8759], file_to_save='_results2.txt')
